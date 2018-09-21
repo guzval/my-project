a8f1eba4ada5ec5f2a7e5f710b44d4ddd8e79f45
Juan Jose <juan.guzval@gmail.com>
2018-09-21 12:59:04 -0500

    añadir template para crear y editar usuarios

--- src/components/administration/AdminUsersDialog.vue
+++ src/components/administration/AdminUsersDialog.vue
@@ -1,5 +1,80 @@
 <template>
+    <v-dialog v-model='usersDialog' max-width="500px">
     
+        <v-btn
+            dark
+            slot="activator"
+            class="indigo lighten-1 white--text text-xs-center mb-2"
+        >
+            {{ $t('admin.usersTable.newUser') }}
+        </v-btn>
+
+        <v-card>
+            <v-card-title>
+                <span class="headline">
+                    {{ $t('admin.usersTable.newUser')}}
+                </span>
+            </v-card-title>
+
+            <v-card-text>
+                <v-container grid-list-md>
+                    <v-layout wrap>
+
+                        <v-flex xs12>
+                            <v-text-field
+                             :label="$t('auth.email')"
+                             v-model="userForEdit.email"
+                             />
+                        </v-flex>
+
+                        <v-spacer />
+
+                        <v-flex xs12>
+                            <v-text-field
+                             :label="$t('auth.username')"
+                             v-model="userForEdit.username"
+                             />
+                        </v-flex>
+
+                    </v-layout>
+                </v-container>
+            </v-card-text>
+            <v-card-actions>
+                
+                <v-spacer />
+
+                <v-btn
+                    color="blue darken-1"
+                    flat
+                    @click="close"
+                >
+                    {{$t('common.close') }}
+                </v-btn>
+
+                <v-btn
+                    v-if="usersDialogEditMode"
+                    color="blue darken-1"
+                    flat
+                    @click="update"
+                >
+                    {{$t('common.update') }}
+                </v-btn>
+
+                <v-btn
+                    v-else
+                    color="blue darken-1"
+                    flat
+                    @click="add"
+                >
+                    {{$t('common.save') }}
+                </v-btn>
+
+            </v-card-actions>
+        </v-card>
+
+
+    </v-dialog>
+
 </template>
 
 <script>
@@ -55,7 +130,7 @@
                     this.close();
                 }
             },
-            ...mapGetters(['userForEdit'])
+            ...mapGetters(['userForEdit', 'usersDialogEditMode'])
         }
     }
 </script>
