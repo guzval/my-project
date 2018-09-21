91483105e1745c3babd3c4edfca511413527710a
Juan Jose <juan.guzval@gmail.com>
2018-09-20 19:53:19 -0500

    config firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,9 @@
+export default
+    {
+        apiKey: "AIzaSyCrKhbvkWfSh6hMO_CsXmGxaPT0ZLlzzhY",
+        authDomain: "dentalwire-37fde.firebaseapp.com",
+        databaseURL: "https://dentalwire-37fde.firebaseio.com",
+        projectId: "dentalwire-37fde",
+        storageBucket: "dentalwire-37fde.appspot.com",
+        messagingSenderId: "572454853716"
+      };
