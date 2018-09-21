359d2176ac2abc0ceeeeb48c881ba45b247cbc4e
Juan Jose <juan.guzval@gmail.com>
2018-09-21 11:55:04 -0500

    añadir navegacion admin

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
  <main>
    <v-app>
-      <v-content>
-        <guest-navigation />
+      <v-content v-if="$store.state.loaded">
+        
+        <guest-navigation v-if="role === 'guest'"/>
+        <admin-navigation v-if="role === 'admin'"/>
+
         <v-container class="mt-3 mb-3">
           <router-view/>
         </v-container>
@@ -10,6 +13,12 @@
         <app-footer/>
 
       </v-content>
+
+      <v-container v-else fill-height>
+        <v-layout flex align-center justify-center>
+          <v-progress-circular indeterminate :size="100" :width="10" color="purple" />
+        </v-layout>
+      </v-container>
    </v-app>
  </main>
 </template>
@@ -17,12 +26,18 @@
 <script>
   import GuestNavigation from '@/navigations/guest';
   import AppFooter from '@/components/Footer';
+  import AdminNavigation from '@/navigations/admin';
+  import {mapGetters} from 'vuex';
   export default {
     components:{
+      AdminNavigation,
       AppFooter,
       GuestNavigation
     },
-    name: 'App'
+    name: 'App',
+    computed: {
+      ...mapGetters(['role'])
+    }
   }
 </script>
 
