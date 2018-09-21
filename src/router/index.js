54bdf229868753baad0e17f40f83843909c7afc5
Juan Jose <juan.guzval@gmail.com>
2018-09-21 02:05:26 -0500

    añadir control acceso no permitido

--- src/router/index.js
+++ src/router/index.js
@@ -6,34 +6,53 @@ import Login from '@/components/Login';
 
 Vue.use(Router)
 
-export default new Router({
+import store from '@/store';
+const beforeEnter = (to, from, next) => {
+  if (store.state.authModule.logged) {
+    next({path: '/'})
+  } else {
+    next ();
+  }
+};
+
+const router = new Router({
   routes: [
     {
       path: '/',
       name: 'Home',
       component: Home,
-      meta: {
-        Auth: false, 
-        title: 'Inicio' 
-      },   
+      meta: { Auth: false, title: 'Inicio' },   
     },
     {
       path: '/register',
       name: 'Register',
       component: Register,
-      meta: {
-        Auth: false, 
-        title: 'Registro' 
-      },   
+      meta: { Auth: false, title: 'Registro' },   
+      beforeEnter: (to, from, next) => beforeEnter (to,from,next)
     },
     {
       path: '/login',
       name: 'Login',
       component: Login,
-      meta: {
-        Auth: false, 
-        title: 'Iniciar sesión' 
-      },   
+      meta: { Auth: false, title: 'Iniciar sesión' },  
+      beforeEnter: (to, from, next) => beforeEnter (to,from,next) 
     },
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title;
+  if (to.meta.Auth && !store.state.authModule.logged && store.state.loaded) {
+    next ({path: '/login'});
+  } else {
+    if (to.meta.role){
+      if (store.state.loaded && (to.meta.role !== store.state.authModule.role)) {
+        next({path: '/'});
+        return;
+      }
+    }
+    next();
+  }
+});
+
+export default router;
